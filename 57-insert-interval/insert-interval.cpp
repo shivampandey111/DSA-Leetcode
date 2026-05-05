02a922aa5eaa1048b85d54cd7308912f7f255a89
Shivam Pandey <shivampandey33033@gmail.com>
2026-05-05 19:18:19 +0530

    Time: 3 ms (29.84%) | Memory: 23.1 MB (5.85%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& interval, vector<int>& nI) {
-        interval.push_back({nI[0], nI[1]});
-        sort(interval.begin(), interval.end());
-
         vector<vector<int>> res;
+        int n = interval.size();
+        vector<vector<int>> newArr;
 
-        int s1 = interval[0][0];
-        int e1 = interval[0][1];
+        bool insert = false; 
+        
+        for(int i = 0; i<interval.size(); i++){
+            if(insert == false && interval[i][0]>=nI[0]){
+                newArr.push_back({nI[0],nI[1]});
+                insert = true;
+            }
+            newArr.push_back({interval[i][0], interval[i][1]});
+        }
+        if(insert==false){
+            newArr.push_back({nI[0], nI[1]});
+        }
 
-        for(int i = 1; i<interval.size(); i++){
-            int s2 = interval[i][0];
-            int e2 = interval[i][1];
+        int s1 = newArr[0][0];
+        int e1 = newArr[0][1];
+
+        for(int i = 1; i<newArr.size(); i++){
+            int s2 = newArr[i][0];
+            int e2 = newArr[i][1];
 
             if(e1>=s2){
                 s1 = s1;
