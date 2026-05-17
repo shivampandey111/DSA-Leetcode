c88d6dbd3a1b510c33811ebe123b58ea83b25d28
Shivam Pandey <shivampandey33033@gmail.com>
2026-05-17 12:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.37%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int mini(vector<int>& nums){
+        int low = 0;
+        int high = nums.size()-1;
+        int mini = 0;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]>=nums[high]){
+                if(nums[mid]<nums[mini])
+                mini = mid;
+                low = mid +1;
+            }
+            else if(nums[mid]<=nums[high]){
+                high = mid -1;
+                if(nums[mini]>nums[mid])
+                mini = mid;
+            }
+        }
+        return mini;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int low = mini(nums);
+        int high = nums.size()-1;
+        int sL = 0;
+        int hH = low -1;
+        if(low==0){
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(nums[mid]==target) return mid;
+                else if(nums[mid]>target) high = mid -1;
+                else{
+                    low = mid +1;
+                }
+            }
+            return -1;
+        }
+        while(low<=high){
+                int mid = (low+high)/2;
+                if(nums[mid]==target) return mid;
+                else if(nums[mid]>target) high = mid -1;
+                else{
+                    low = mid +1;
+                }
+            }
+        
+        while(sL<=hH){
+            int mid = sL + (hH-sL)/2;
+            if(nums[mid]==target){
+                return mid;
+            } 
+            else if(nums[mid]>target) hH = mid -1;
+            else{
+                sL = mid +1;
+            }
+        }
+
+        return -1;
+    }
+};
