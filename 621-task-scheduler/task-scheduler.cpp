05044ee98f30286f1a69761c845c515a04a08112
Shivam Pandey <shivampandey33033@gmail.com>
2026-05-28 19:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 38.3 MB (66.96%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -5,32 +5,31 @@ public:
         for(int i = 0; i<tasks.size(); i++){
             freq[tasks[i]]++;
         }
-        int maxi = 0;
-        for(auto val:freq){
-            maxi = max(maxi, val.second);
-        }
-        // priority_queue<pair<char, int>> pq;
+        // int maxi = 0;
         // for(auto val:freq){
-        //     int first = val.second;
-        //     char second = val.first;
-        //     pair<int, char> curr = {first, second};
-        //     pq.push(curr);
+        //     maxi = max(maxi, val.second);
         // }
-        // int maxi = pq.top().first;
-
-        int countMax = 0;
+        priority_queue<pair<int, char>> pq;
         for(auto val:freq){
-            if(val.second==maxi){
-                countMax++;
-            }
+            int first = val.second;
+            char second = val.first;
+            pair<int, char> curr = {first, second};
+            pq.push(curr);
         }
-        // while(!pq.empty()){
-        //     int curr = pq.top().first;
-        //     pq.pop();
-        //     if(curr==maxi){
+        int countMax = 0;
+        int maxi = pq.top().first;
+        // for(auto val:freq){
+        //     if(val.second==maxi){
         //         countMax++;
         //     }
         // }
+         while(!pq.empty()){
+             int curr = pq.top().first;
+             pq.pop();
+             if(curr==maxi){
+                 countMax++;
+             }
+         }
         int res =max((int)tasks.size(), (maxi-1)*(n+1)+countMax);
         return res;
     }
