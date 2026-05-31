6bb13fadcacd634d5069eae7cab9436f7e7a803e
Shivam Pandey <shivampandey33033@gmail.com>
2026-05-31 18:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (5.45%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        stack<char> rev;
-
-        for(int i =0; i<s.size(); i++){
-            rev.push(s[i]);
+    void func(vector<char>& arr, int idx, vector<char>& temp){
+        if(idx<0) return;
+        if(idx==0){
+            temp.push_back(arr[idx]);
+            return;
         }
-        int i=0;
-            while((!rev.empty()) && i<s.size()){
-                s[i] = rev.top();
-                rev.pop();
-                i++;
-            }
+        temp.push_back(arr[idx]);
+        func(arr, idx-1, temp);
+    }
+
+    void reverseString(vector<char>& s) {
+        if(s.size()<=0) return;
+        int idx = s.size()-1;
+        vector<char> temp;
+        func(s, idx, temp);
+        s = temp;
+
+        // stack<char> rev;
+
+        // for(int i =0; i<s.size(); i++){
+        //     rev.push(s[i]);
+        // }
+        // int i=0;
+        //     while((!rev.empty()) && i<s.size()){
+        //         s[i] = rev.top();
+        //         rev.pop();
+        //         i++;
+        //     }
         
     }
 };
