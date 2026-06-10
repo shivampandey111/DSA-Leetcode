c574fa7aa1298f8d5b1397dd5770ffdb79d98d1a
Shivam Pandey <shivampandey33033@gmail.com>
2026-06-10 21:59:35 +0530

    Time: 23 ms (56.55%) | Memory: 23.6 MB (33.97%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* ans = NULL;
+    int func(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL) return 0;
+        int left = 0;
+        int right = 0;
+        if(root->val>p->val && root->val<q->val){
+            left = func(root->left, p, q);
+            right = func(root->right, p, q);
+        }
+        else if(root->val>p->val || root->val>q->val){
+            left = func(root->left, p, q);
+        }
+        else if(root->val<p->val || root->val<q->val){
+            right = func(root->right, p, q);
+        }
+        int self = 0;
+        if(root->val == p->val || root->val == q->val){
+            self++;
+        }
+        int total = left + self + right;
+        if(total==2 && ans ==NULL){
+            ans = root;
+            return total;
+        }
+        return total;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val>q->val){
+            swap(p,q);
+        }
+        func(root, p, q);
+        return ans;
+    }
+};
