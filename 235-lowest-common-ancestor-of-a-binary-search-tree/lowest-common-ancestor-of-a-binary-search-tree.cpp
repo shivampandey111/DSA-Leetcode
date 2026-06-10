9ddab6da43143db9354805055b48a41a2ffe5550
Shivam Pandey <shivampandey33033@gmail.com>
2026-06-10 23:27:10 +0530

    Time: 21 ms (71.83%) | Memory: 23.4 MB (91.03%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     TreeNode* ans = NULL;
-    int func(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root==NULL) return 0;
-        int left = 0;
-        int right = 0;
-        if(root->val>p->val && root->val<q->val){
-            left = func(root->left, p, q);
-            right = func(root->right, p, q);
-        }
-        else if(root->val>p->val || root->val>q->val){
-            left = func(root->left, p, q);
-        }
-        else if(root->val<p->val || root->val<q->val){
-            right = func(root->right, p, q);
-        }
-        int self = 0;
-        if(root->val == p->val || root->val == q->val){
-            self++;
-        }
-        int total = left + self + right;
-        if(total==2 && ans ==NULL){
+    void func(TreeNode* root, TreeNode* p, TreeNode*q){
+        if(root==NULL) return;
+        if(root->val==p->val || root->val==q->val){
             ans = root;
-            return total;
+            return;
         }
-        return total;
+        if(root->val<p->val){
+            func(root->right, p, q);
+        }
+        else if(root->val>q->val){
+            func(root->left, p, q);
+        }
+        else{
+            ans = root;
+            return;
+        }
     }
+
+
+    // int func(TreeNode* root, TreeNode* p, TreeNode* q){
+    //     if(root==NULL) return 0;
+    //     int left = 0;
+    //     int right = 0;
+    //     if(root->val>p->val && root->val<q->val){
+    //         left = func(root->left, p, q);
+    //         right = func(root->right, p, q);
+    //     }
+    //     else if(root->val>p->val || root->val>q->val){
+    //         left = func(root->left, p, q);
+    //     }
+    //     else if(root->val<p->val || root->val<q->val){
+    //         right = func(root->right, p, q);
+    //     }
+    //     int self = 0;
+    //     if(root->val == p->val || root->val == q->val){
+    //         self++;
+    //     }
+    //     int total = left + self + right;
+    //     if(total==2 && ans ==NULL){
+    //         ans = root;
+    //         return total;
+    //     }
+    //     return total;
+    // }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(p->val>q->val){
             swap(p,q);
