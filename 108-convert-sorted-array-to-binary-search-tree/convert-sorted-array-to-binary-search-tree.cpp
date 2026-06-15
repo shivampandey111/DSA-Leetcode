ff066c30112bac4acaf938765e27ad1f0d8600ad
Shivam Pandey <shivampandey33033@gmail.com>
2026-06-15 23:51:52 +0530

    Time: 4 ms (34.78%) | Memory: 22.9 MB (54.74%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -5,15 +5,11 @@ public:
         if(low>high) return NULL;
         int mid = (low+high)/2;
         TreeNode* node = new TreeNode(nums[mid]);
-        int id = freq[node->val];
-        node->left = func(nums, low, id-1);
-        node->right = func(nums, id+1, high);
+        node->left = func(nums, low, mid-1);
+        node->right = func(nums, mid+1, high);
         return node;
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        for(int i = 0; i<nums.size(); i++){
-            freq[nums[i]] = i;
-        }
         return func(nums, 0, nums.size()-1);
     }
 };
