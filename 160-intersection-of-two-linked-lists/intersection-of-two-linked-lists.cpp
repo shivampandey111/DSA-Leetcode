8f59451781b09ec6edbec52e43383bba39b89497
Shivam Pandey <shivampandey33033@gmail.com>
2026-07-05 01:59:36 +0530

    Time: 56 ms (60.09%) | Memory: 23.9 MB (56.67%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,15 +1,62 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> freq;
-        while(headA){
-            freq.insert(headA);
-            headA = headA->next;
+        int t1 = 1;
+        int t2 = 1;
+        ListNode* f1 = headA;
+        ListNode* f2 = headB;
+        while(f1->next){
+            f1 = f1->next;
+            t1++;
         }
-        while(headB){
-           if(freq.count(headB)) return headB;
-           headB = headB->next;
+        while(f2->next){
+            f2= f2->next;
+            t2++;
         }
-        return nullptr;
+        f1 = headA;
+        f2 = headB;
+        ListNode* res = nullptr;
+        int i = 0;
+        int diff = abs(t1-t2);
+        if(t1>t2){
+            while(i<diff){
+                f1 = f1->next;
+                i++;
+            }
+            while(f1!=nullptr && f2!=nullptr){
+                if(f1==f2){
+                    res = f1;
+                    break;
+                }
+                f1 = f1->next;
+                f2 = f2->next;
+            }
+        }
+        else if(t1<t2){
+            while(i<diff){
+                f2 = f2->next;
+                i++;
+            }
+            while(f2!=nullptr && f1!=nullptr){
+                if(f1==f2){
+                    res = f2;
+                    break;
+                }
+                f1 = f1->next;
+                f2 = f2->next;
+            }
+        }
+        else{
+            while(f1!=nullptr && f2!=nullptr){
+                if(f1==f2){
+                    res = f1;
+                    break;
+                }
+                f1 = f1->next;
+                f2 = f2->next;
+            }
+        }
+        return res;
+
     }
 };
