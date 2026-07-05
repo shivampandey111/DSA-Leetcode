5119a48eae0f4a285ef46840f31e5f773dbcdeeb
Shivam Pandey <shivampandey33033@gmail.com>
2026-07-05 18:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.07%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
         int low = 0;
-        int mid = 1;
-        if(arr.size()==1) return; 
-        while(mid<arr.size()){
-            if(arr[low]==0 & arr[mid]!=0){
-                swap(arr[mid], arr[low]);
+        int high = 0;
+        while(high<arr.size()){
+            if(arr[high]!=0){
+                swap(arr[low], arr[high]);
                 low++;
-                mid++;
+                high++;
             }
             else{
-                if(arr[low]==0 & arr[mid]==0){
-                    mid++;
-                }
-                else{
-                    low++;
-                    mid++;
-                }
+                high++;
             }
         }
+        // int low = 0;
+        // int mid = 1;
+        // if(arr.size()==1) return; 
+        // while(mid<arr.size()){
+        //     if(arr[low]==0 & arr[mid]!=0){
+        //         swap(arr[mid], arr[low]);
+        //         low++;
+        //         mid++;
+        //     }
+        //     else{
+        //         if(arr[low]==0 & arr[mid]==0){
+        //             mid++;
+        //         }
+        //         else{
+        //             low++;
+        //             mid++;
+        //         }
+        //     }
+        // }
     }
 };
