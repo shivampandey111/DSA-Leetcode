21ea4fdaa0fbf3e8c8a648e441b2370b06defbb3
Shivam Pandey <shivampandey33033@gmail.com>
2026-07-09 19:57:10 +0530

    Time: 67 ms (57.63%) | Memory: 14.4 MB (48.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int fun(vector<int>& nums, int n, int i, int prev, vector<vector<int>>& dp){
-        if(i==n) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        if(prev==-1 || nums[i]>nums[prev]){
-            int c1 = 1 + fun(nums, n, i+1, i, dp);
-            int c2 = fun(nums, n, i+1, prev, dp);
+    // int fun(vector<int>& nums, int n, int i, int prev, vector<vector<int>>& dp){
+    //     if(i==n) return 0;
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     if(prev==-1 || nums[i]>nums[prev]){
+    //         int c1 = 1 + fun(nums, n, i+1, i, dp);
+    //         int c2 = fun(nums, n, i+1, prev, dp);
 
-            return dp[i][prev+1] = max(c1, c2);
-        }
-        return dp[i][prev+1] = fun(nums, n, i+1, prev, dp);
-    }
+    //         return dp[i][prev+1] = max(c1, c2);
+    //     }
+    //     return dp[i][prev+1] = fun(nums, n, i+1, prev, dp);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1);
-        for(int i = 0; i<=n; i++){
-            vector<int> temp(n+1, -1);
-            dp[i] = temp;
+        // vector<vector<int>> dp(n+1);
+        // for(int i = 0; i<=n; i++){
+        //     vector<int> temp(n+1, -1);
+        //     dp[i] = temp;
+        // }
+        // return fun(nums, n, 0, -1, dp);
+        vector<int> dp(n);
+        for(int i = 0; i<n; i++){
+            dp[i] = 1;
+            for(int j = 0; j<i; j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
         }
-        return fun(nums, n, 0, -1, dp);
+        int res = 0;
+        for(int i = 0; i<n; i++){
+            res = max(res, dp[i]);
+        }
+        return res;
     }
 };
