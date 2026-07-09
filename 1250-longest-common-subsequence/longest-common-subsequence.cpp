c11089d1f21198cf38ec4b51afc61b50705d9b5f
Shivam Pandey <shivampandey33033@gmail.com>
2026-07-09 23:29:33 +0530

    Time: 35 ms (33.28%) | Memory: 45.2 MB (6.08%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int fun(string& text1, string& text2, int n, int m, int i, int j, vector<vector<int>>& dp){
-        if(i==n || j==m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]) return dp[i][j] = 1 + fun(text1, text2, n, m, i+1, j+1, dp);
+    // int fun(string& text1, string& text2, int n, int m, int i, int j, vector<vector<int>>& dp){
+    //     if(i==n || j==m) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(text1[i]==text2[j]) return dp[i][j] = 1 + fun(text1, text2, n, m, i+1, j+1, dp);
         
-            int c1 = fun(text1, text2, n, m, i+1, j, dp);
-            int c2 = fun(text1, text2, n, m, i, j+1, dp);
-            return dp[i][j] = max(c1, c2) ;
+    //         int c1 = fun(text1, text2, n, m, i+1, j, dp);
+    //         int c2 = fun(text1, text2, n, m, i, j+1, dp);
+    //         return dp[i][j] = max(c1, c2) ;
         
-    }
+    // }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
@@ -18,6 +18,21 @@ public:
             vector<int> temp(m+1, -1);
             dp[i] = temp;
         }
-        return fun(text1, text2, n, m, 0, 0, dp);
+        for(int i = 0; i<=m; i++) dp[n][i] = 0;
+        for(int i = 0; i<=n; i++) dp[i][m] = 0;
+        for(int i = n-1; i>=0; i--){
+            for(int j = m-1; j>=0; j--){
+                if(text1[i]==text2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    int c1 = dp[i+1][j];
+                    int c2 = dp[i][j+1];
+                    dp[i][j] = max(c1, c2);
+                }
+            }
+        }
+        return dp[0][0];
+        // return fun(text1, text2, n, m, 0, 0, dp);
     }
 };
